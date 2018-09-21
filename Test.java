36c5c19596f42528037360fd25c98cf2b5379ff3
Huynh Nga <41855929+ngahohuynh@users.noreply.github.com>
2018-09-21 16:19:49 +0700

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,21 @@
+package com.example.chemistry24.models;
+
+public class Test {
+
+    private int idTest;
+
+    public Test(int idTest) {
+
+        this.idTest = idTest;
+    }
+
+    public int getIdTest() {
+
+        return idTest;
+    }
+
+    public void setIdTest(int idTest) {
+
+        this.idTest = idTest;
+    }
+}
